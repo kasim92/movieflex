3d63d1a03bef92a44cd46fd9f5d46d13f02b96d6
Kasim Mohammed <33530853+kasim92@users.noreply.github.com>
2022-02-07 23:31:34 +0300

    changes h

--- components/BaseImage.vue
+++ components/BaseImage.vue
@@ -1,15 +1,12 @@
 <template>
   <div>
-    <v-img
-      :src="img"
-      v-bind="$attrs"
-      v-on="$listeners"
-    >
+    <v-img :src="img" v-bind="$attrs" v-on="$listeners">
       <template v-slot:placeholder>
-        <v-row  align="center" justify="center">
+        <v-row align="center" justify="center">
           <v-progress-circular
             indeterminate
             color="grey lighten-5"
+            aria-label="loading"
           ></v-progress-circular>
         </v-row>
       </template>
